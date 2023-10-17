4ca15e317991e373854c0e94ce251e3378dfbdb5
Alex2 <aleksander1802@gmail.com>
2023-10-17 16:29:27 +0300

    feat: practice of passing parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+    title: string;
+    text: string;
+    id?: number;
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+    posts: Post[] = [
+        {
+            id: 1,
+            title: 'Angular components',
+            text: "I'm still learning",
+        },
+        {
+            id: 2,
+            title: 'Next article',
+            text: 'About directives and pipes',
+        },
+    ];
+}
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../app.component';
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+    selector: 'app-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.scss'],
 })
 export class PostComponent {
-
+    @Input() post: Post | undefined;
 }
