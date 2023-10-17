0e5bcc8d6affaaa9281b86af172bc47e12beb062
Alex2 <aleksander1802@gmail.com>
2023-10-17 15:54:28 +0300

    feat: added global styles, added post and post-form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './app/post/post.component';
+import { FormsModule } from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
-    declarations: [AppComponent, PostComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, PostFormComponent, PostComponent],
+    imports: [BrowserModule, AppRoutingModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
