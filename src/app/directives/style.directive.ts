0da22d0afbbe5970fe4ba48d608a8f67d00356a7
Alex2 <aleksander1802@gmail.com>
2023-10-17 20:36:28 +0300

    feat: added some dynamics to the custom directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,18 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({ selector: '[appStyle]' })
 export class StyleDirective {
-    constructor(private elRef: ElementRef, private renderer: Renderer2) {
-        this.renderer.setStyle(this.elRef.nativeElement, 'color', 'darkblue');
+    constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+    @HostListener('click', ['$event']) onClick(el: Element) {
+        console.log(el);
+    }
+
+    @HostListener('mouseenter') onEnter() {
+        this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue');
+    }
+
+    @HostListener('mouseleave') onLeave() {
+        this.renderer.setStyle(this.elRef.nativeElement, 'color', null);
     }
 }
