4f08bf5a8df854cf1962778da81afefc7b22da80
Alex2 <aleksander1802@gmail.com>
2023-10-18 22:38:16 +0300

    feat: added custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { MultByPipe } from './pipes/mult-by.pipe';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, MultByPipe],
     imports: [BrowserModule, AppRoutingModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
