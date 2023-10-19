aa1d2c520e4cbe051e563b26e862492e31e329bb
Alex2 <aleksander1802@gmail.com>
2023-10-19 22:54:10 +0300

    feat: binding services, practice with services is over

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LogService } from './log.service';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class AppCounterService {
     counter = 0;
 
+    constructor(public logService: LogService) {}
+
     increase() {
+        this.logService.log('increase counter');
         this.counter += 1;
     }
 
     decrease() {
+        this.logService.log('decrease counter');
         this.counter -= 1;
     }
 }
