991d15d20f953a469eef482ea9dfdde8224124ad
Alex2 <aleksander1802@gmail.com>
2023-10-21 20:41:55 +0300

    feat: added custom ngmodel, practice with forms is over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {
-    FormArray, FormControl, FormGroup, Validators,
-} from '@angular/forms';
-import { MyValidators } from './my.validators';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    form!: FormGroup;
+export class AppComponent {
+    appState = 'on';
 
-    ngOnInit() {
-        this.form = new FormGroup({
-            email: new FormControl(
-                '',
-                [
-                    Validators.email,
-                    Validators.required,
-                    MyValidators.restrictedEmail,
-                ],
-                [MyValidators.uniqueEmail()],
-            ),
-            password: new FormControl(null, [
-                Validators.required,
-                Validators.minLength(6),
-            ]),
-            address: new FormGroup({
-                country: new FormControl('ru'),
-                city: new FormControl('', Validators.required),
-            }),
-            skills: new FormArray<FormGroup>([]),
-        });
-    }
-
-    submit() {
-        if (this.form.valid) {
-            console.log('Form submited', this.form);
-            const formData = { ...this.form.value };
-            console.log('Form value', formData);
-
-            this.form.reset();
-        }
-    }
-
-    setCapital() {
-        const cityMap = {
-            ru: 'Москва',
-            by: 'Минск',
-            ua: 'Киев',
-        };
-
-        const cityKey: keyof typeof cityMap = this.form
-            .get('address')
-            ?.get('country')?.value;
-        const city = cityMap[cityKey];
-
-        this.form.patchValue({
-            address: { city },
-        });
-    }
-
-    addSkill() {
-        const control = new FormControl('', Validators.required);
-
-        (this.form.get('skills') as FormArray).push(control);
-    }
-
-    get skillsFormArray() {
-        return this.form.get('skills') as FormArray;
+    handleChange() {
+        console.log(this.appState);
     }
 }
