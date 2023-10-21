b07a6d982250f830f47cd9fd355f126b0f52698b
Alex2 <aleksander1802@gmail.com>
2023-10-21 23:07:32 +0300

    feat: start practice with http, added jsonplaceholder response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+export interface Todo {
+    title: string;
+    completed: boolean;
+    id?: number;
+}
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    appState = 'on';
+export class AppComponent implements OnInit {
+    constructor(private http: HttpClient) {}
 
-    handleChange() {
-        console.log(this.appState);
+    todos: Todo[] = [];
+
+    ngOnInit() {
+        this.http
+            .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+            .subscribe((response) => {
+                this.todos = response;
+            });
     }
 }
