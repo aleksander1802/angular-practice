e1989b28e18e0a6e2de0518ba02fe959e5f3a932
Alex2 <aleksander1802@gmail.com>
2023-10-22 19:42:36 +0300

    feat: added parameter processing

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Post, PostsService } from '../posts.service';
 
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
     styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+    post!: Post;
 
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private postsService: PostsService,
+    ) {}
+
+    ngOnInit() {
+        this.route.params.subscribe((params: Params) => {
+            const postId = params['id'];
+            const foundPost = this.postsService.getById(+postId);
+            if (foundPost) {
+                this.post = foundPost;
+            } else {
+                throw new Error('Posts not found');
+            }
+        });
+    }
+
+    loadPost() {
+        this.router.navigate(['/posts', 44]);
+    }
 }
