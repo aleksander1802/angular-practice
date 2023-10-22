a1878468291dc2d784cb0d40f9662c70841499b0
Alex2 <aleksander1802@gmail.com>
2023-10-22 14:07:24 +0300

    feat: added posts with post http method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,11 +17,31 @@ export class AppComponent implements OnInit {
 
     todos: Todo[] = [];
 
+    todoTitle = '';
+
     ngOnInit() {
         this.http
             .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
             .subscribe((response) => {
                 this.todos = response;
+            });
+    }
+
+    addTodo() {
+        if (!this.todoTitle.trim()) {
+            return;
+        }
+
+        const newTodo: Todo = {
+            title: this.todoTitle,
+            completed: false,
+        };
+
+        this.http
+            .post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+            .subscribe((response) => {
+                this.todos.push(response);
+                this.todoTitle = '';
             });
     }
 }
