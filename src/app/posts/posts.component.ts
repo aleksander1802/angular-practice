3777276f74bcee22cc7c1658f3d206307190cd22
Alex2 <aleksander1802@gmail.com>
2023-10-22 20:12:42 +0300

    feat: practice with params

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PostsService } from '../posts.service';
 
 @Component({
@@ -6,6 +7,31 @@ import { PostsService } from '../posts.service';
     templateUrl: './posts.component.html',
     styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent {
-    constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit {
+    showIds = false;
+
+    constructor(
+        public postsService: PostsService,
+        private route: ActivatedRoute,
+        private router: Router,
+    ) {}
+
+    ngOnInit() {
+        this.route.queryParams.subscribe((params) => {
+            this.showIds = !!params['showIds'];
+        });
+
+        this.route.fragment.subscribe((fragmant) => {
+            console.log(fragmant);
+        });
+    }
+
+    showIdsProgram() {
+        this.router.navigate(['/posts'], {
+            queryParams: {
+                showIds: true,
+            },
+            fragment: 'program-fragment',
+        });
+    }
 }
