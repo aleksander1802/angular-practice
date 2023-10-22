dbe412a99864a8fbe5f8b27b77daaa04417cc347
Alex2 <aleksander1802@gmail.com>
2023-10-22 18:25:53 +0300

    feat: started practice with routing, added base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Todo, TodosService } from './services/todos.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    constructor(private todosService: TodosService) {}
-
-    todos: Todo[] = [];
-
-    todoTitle = '';
-
-    loading = false;
-
-    error = '';
-
-    ngOnInit() {
-        this.fetchTodos();
-    }
-
-    addTodo() {
-        if (!this.todoTitle.trim()) {
-            return;
-        }
-
-        const newTodo: Todo = {
-            title: this.todoTitle,
-            completed: false,
-        };
-
-        this.todosService.addTodo(newTodo).subscribe((todo) => {
-            this.todos.push(todo);
-            this.todoTitle = '';
-        });
-    }
-
-    fetchTodos() {
-        this.loading = true;
-        this.todosService.fetchTodos().subscribe(
-            (response) => {
-                this.todos = response;
-                this.loading = false;
-            },
-            (error) => {
-                this.error = error.message;
-            },
-        );
-    }
-
-    removeTodo(id?: number) {
-        this.todosService.removeTodo(id).subscribe(() => {
-            this.todos = this.todos.filter((todo) => todo.id !== id);
-        });
-    }
-
-    completeTodo(id?: number) {
-        return this.todosService.completeTodo(id).subscribe(
-            (todo) => {
-                console.log(todo);
-                this.todos.find((t) => t.id === todo.id)!.completed = true;
-            },
-            (error) => {
-                this.error = error.message;
-            },
-        );
-    }
-}
+export class AppComponent {}
