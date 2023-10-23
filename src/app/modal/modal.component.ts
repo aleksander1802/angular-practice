c2bbf7da929ba48001a5e116f799ff7af22274b5
Alex2 <aleksander1802@gmail.com>
2023-10-23 22:38:00 +0300

    feat: started practice with dynamic components, added modal

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,14 @@
+import {
+    Component, EventEmitter, Input, Output,
+} from '@angular/core';
+
+@Component({
+    selector: 'app-modal',
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent {
+    @Input() title = 'Default title';
+
+    @Output() closeModal = new EventEmitter();
+}
--- src/app/shared/color.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, HostBinding } from '@angular/core';
-
-@Directive({
-    selector: '[appColor]',
-})
-export class ColorDirective {
-    @HostBinding('style.color') color = '#aaa';
-}
