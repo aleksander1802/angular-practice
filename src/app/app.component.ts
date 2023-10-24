49375daf828ed2613b8065e863661f9e7f8ad5a6
Alex2 <aleksander1802@gmail.com>
2023-10-24 16:18:55 +0300

    feat: added animation state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    animations: [
+        trigger('box', [
+            state('start', style({ background: 'cyan' })),
+            state(
+                'end',
+                style({ background: 'crimson', transform: 'scale(1.2)' }),
+            ),
+        ]),
+    ],
 })
-export class AppComponent {}
+export class AppComponent {
+    boxState = 'start';
+
+    animate() {
+        this.boxState = this.boxState === 'start' ? 'end' : 'start';
+    }
+}
