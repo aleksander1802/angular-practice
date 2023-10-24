94789a70e3f1c4be6c152f4aac47b22884a56eb7
Alex2 <aleksander1802@gmail.com>
2023-10-24 18:04:32 +0300

    feat: added complex animation transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,21 @@ import { Component } from '@angular/core';
                 'end',
                 style({ background: 'crimson', transform: 'scale(1.2)' }),
             ),
+            state(
+                'special',
+                style({
+                    background: 'orange',
+                    transform: 'scale(0.5)',
+                    borderRadius: '50%',
+                }),
+            ),
             transition('start => end', animate(500)),
             transition('end => start', animate('800ms ease-in-out')),
+            transition('special <=> *', [
+                style({ background: 'green' }),
+                animate(1000, style({ background: 'black' })),
+                animate(750),
+            ]),
         ]),
     ],
 })
