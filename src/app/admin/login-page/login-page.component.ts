fe99cf00cf3b65551d2a5dae99415e4fff0c769a
Alex2 <aleksander1802@gmail.com>
2023-10-26 23:40:19 +0300

    feat: updated admin login submit method

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/interfaces/user.interface';
 
 @Component({
     selector: 'app-login-page',
@@ -26,6 +27,12 @@ export class LoginPageComponent implements OnInit {
         if (this.form.invalid) {
             return false;
         }
+
+        const user: User = {
+            email: this.form.value.email,
+            password: this.form.value.password,
+        };
+
         return true;
     }
 }
