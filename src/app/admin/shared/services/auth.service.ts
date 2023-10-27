a8cbbb2b5d978fed2e04aa22840105d52a8cd19b
Alex2 <aleksander1802@gmail.com>
2023-10-27 23:02:11 +0300

    feat: added token autorization

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from 'src/app/shared/interfaces/user.interface';
+import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs';
 
 @Injectable()
 export class AuthService {
@@ -11,12 +13,19 @@ export class AuthService {
     }
 
     login(user: User) {
-        return this.http.post('', user);
+        return this.http
+            .post(
+                `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${environment.apiKey}`,
+                user,
+            )
+            .pipe(tap(this.setToken));
     }
 
     logout() {}
 
     isAuthenticated() {}
 
-    private setToken() {}
+    private setToken(response: unknown) {
+        console.log(response);
+    }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { Environment } from './interface';
+
+export const environment: Environment = {
+    production: false,
+    apiKey: 'AIzaSyC3SXQEbHWOSiz9zao1qEz9GOtu',
+};
