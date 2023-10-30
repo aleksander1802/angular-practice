cd756b1cf01b5bb72843acac251e21d11523a7d2
Alex2 <aleksander1802@gmail.com>
2023-10-30 19:39:21 +0300

    feat: added display of posts from the server

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostsService } from '../admin/shared/services/posts.service';
+import { Post } from '../shared/interfaces/form.interface';
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+    posts$: Observable<Post[]> = new Observable();
 
+    constructor(private postsService: PostsService) {}
+
+    ngOnInit() {
+        this.posts$ = this.postsService.getAll();
+    }
 }
