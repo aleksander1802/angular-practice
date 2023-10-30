d3807e34b8244d5cfbfabfac283f3e9324706627
Alex2 <aleksander1802@gmail.com>
2023-10-30 22:13:37 +0300

    feat: added a detailed display of the post

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { PostsService } from '../admin/shared/services/posts.service';
+import { Post } from '../shared/interfaces/form.interface';
 
 @Component({
     selector: 'app-post-page',
     templateUrl: './post-page.component.html',
     styleUrls: ['./post-page.component.scss'],
 })
-export class PostPageComponent {}
+export class PostPageComponent implements OnInit {
+    post$: Observable<Post> = new Observable();
+
+    constructor(
+        private route: ActivatedRoute,
+        private postsService: PostsService,
+    ) {}
+
+    ngOnInit() {
+        this.post$ = this.route.params.pipe(
+            switchMap((params: Params) => this.postsService.getById(params['id'])),
+        );
+    }
+}
